7e44667868474cb0f8b2b160e20319e2c4759ad0
ntarpey <nathan.tarpey@gmail.com>
2014-06-08 20:30:45 -0400

    Fix datatables

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require dataTables/jquery.dataTables
 //= require jquery_ujs
 //= require turbolinks
 //= require underscore
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,8 @@ Lightsoutmap9::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
+
+
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
@@ -28,6 +30,10 @@ Lightsoutmap9::Application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
+
+  config.assets.precompile += %w( *.js *.css )
+
+  config.assets.enabled = true
 
   # Generate digests for assets URLs.
   config.assets.digest = true
